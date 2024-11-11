bebf68461ac138a83dd0faeea8adae6d1cd53b7e
daixll <dxlcq@outlook.com>
2024-11-11 21:27:57 +0800

    保存图像

--- data/3-task/get_and_save_ss_ros/src/get_and_save/src/img.cpp
+++ data/3-task/get_and_save_ss_ros/src/get_and_save/src/img.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <cv_bridge/cv_bridge.h>         // 用于转换 ROS 图像到 OpenCV
 #include <opencv2/highgui/highgui.hpp>   // 图像交互界面，显示图像
 #include <ros/ros.h>                     // ROS 依赖
@@ -22,7 +23,23 @@ void img(const sensor_msgs::ImageConstPtr &msg) {
     } else {
       ROS_ERROR("Unsupported image encoding: %s", msg->encoding.c_str());
     }
-    cv::waitKey(1);
+
+    // 获取当前日期及时间，精确到毫秒
+    auto now = std::chrono::system_clock::now();
+    std::time_t now_time = std::chrono::system_clock::to_time_t(now);
+    std::tm now_tm = *std::localtime(&now_time);
+
+    std::ostringstream oss;
+    oss << std::put_time(&now_tm, "%Y%m%d%H%M%S");
+    std::string date_time = oss.str();
+
+    // 保存图像至当前目录
+    cv::imwrite(date_time + ".jpg", cv_image_ptr->image);
+
+    ROS_INFO("Saved image %s.jpg", date_time.c_str());
+
+    // 每 1 秒刷新一次窗口
+    cv::waitKey(1000);
   } catch (cv_bridge::Exception &e) {
     ROS_ERROR("cv_bridge exception: %s", e.what());
   }
@@ -37,7 +54,7 @@ int main(int argc, char **argv) {
   // 创建要显示的窗口
   cv::namedWindow(IMG_WINDOW, cv::WINDOW_AUTOSIZE);
 
-  // 订阅图像话题，注册回调函数，每 10 毫秒调用一次
+  // 订阅图像话题，注册回调函数，消息队列长度为 10
   ros::Subscriber camera_sub = n.subscribe("/camera/color/image_raw", 10, img);
 
   // 循环等待回调函数
